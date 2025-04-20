8abd0bbe5cd5e42e8bb641fc32440ec90ae6a25f
Jyothsna A Terli <jyothsnaaterli@Jyothsnas-MacBook-Pro.local>
2025-04-19 20:17:52 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('AWS compute devops prep'));
+app.get('/', (req, res) => res.send('AWS compute Devops Test Change'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
